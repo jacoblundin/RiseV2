8e76c451ebc5fa26fb5fb07d2f7ebc1b1273d20b
Fredrik Jeppsson <fredrik.m.jeppsson@gmail.com>
2021-02-24 11:46:01 +0100

    refactored PlayerInfoPanel and PropertyWindow

--- src/View/EastGUI/PlayerInfoPanel.java
+++ src/View/EastGUI/PlayerInfoPanel.java
@@ -14,86 +14,76 @@ import Model.player.PlayerList;
 /**
  * This class is used to show information about the players
  * and the current properties in tabs from property window
- * 
- * @author Abdulkhuder Muhammad, Sebastian Viro.
  *
+ * @author Abdulkhuder Muhammad, Sebastian Viro.
  */
 public class PlayerInfoPanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+    private JLabel lblName = new JLabel("");
+    private JLabel lblRank = new JLabel("");
+    private JLabel lblGold = new JLabel("");
+    private JLabel lblNetworth = new JLabel("");
+    private JPanel p1 = new JPanel();
+    private JPanel p2 = new JPanel();
+    private JPanel p3 = new JPanel();
+    private JPanel p4 = new JPanel();
+    private PropertyWindow propertyWindow;
+    private Font font = new Font("ALGERIAN", Font.PLAIN, 18);
 
-	private static final long serialVersionUID = 1L;
+    /**
+     * @param playerList
+     * @param playernbr  Model.player list is used to get the players to display correct information
+     *                   playernbr is to specify what Model.player
+     */
+    public PlayerInfoPanel(PlayerList playerList, int playernbr) {
+        setPreferredSize(new Dimension(345, 860));
+        p1.setBounds(10, 5, 330, 50);
+        setBackground(Color.DARK_GRAY);
 
-	private JLabel lblName = new JLabel("");
-	private JLabel lblRank = new JLabel("");
-	private JLabel lblGold = new JLabel("");
-	private JLabel lblNetworth = new JLabel("");
-	private JPanel p1 = new JPanel();
-	private JPanel p2 = new JPanel();
-	private JPanel p3 = new JPanel();
-	private JPanel p4 = new JPanel();
-	
-	private PropertyWindow propertyWindow = new PropertyWindow();
+        p1.setBackground(playerList.getPlayerFromIndex(playernbr).getPlayerColor());
+        p1.setBorder(BorderFactory.createLineBorder(Color.black));
+        p2.setBounds(10, 55, 330, 50);
+        p2.setBorder(BorderFactory.createLineBorder(Color.black));
+        p3.setBounds(10, 105, 330, 50);
+        p3.setBorder(BorderFactory.createLineBorder(Color.black));
 
-	private Font font = new Font("ALGERIAN", Font.PLAIN, 18);
+        p4.setBounds(10, 154, 330, 50);
+        p4.setBorder(BorderFactory.createLineBorder(Color.black));
+        setLayout(null);
 
-	/**
-	 * @param playerList
-	 * @param playernbr
-	 * 
-	 * Model.player list is used to get the players to display correct information
-	 * playernbr is to specify what Model.player
-	 */
-	public PlayerInfoPanel(PlayerList playerList, int playernbr) {
+        lblName.setText(playerList.getPlayerFromIndex(playernbr).getName().toUpperCase());
+        lblName.setFont(font);
+        lblName.setHorizontalAlignment(SwingConstants.CENTER);
+        lblName.setOpaque(false);
+        lblName.setForeground(Color.white);
+        lblName.setBackground(playerList.getPlayerFromIndex(playernbr).getPlayerColor());
 
-		setPreferredSize(new Dimension(345, 860));
-		p1.setBounds(10, 5, 330, 50);
-		setBackground(Color.DARK_GRAY);
+        p1.add(lblName);
+        add(p1);
 
-		p1.setBackground(playerList.getPlayerFromIndex(playernbr).getPlayerColor());
-		p1.setBorder(BorderFactory.createLineBorder(Color.black));
-		p2.setBounds(10, 55, 330, 50);
-		p2.setBorder(BorderFactory.createLineBorder(Color.black));
-		p3.setBounds(10, 105, 330, 50);
-		p3.setBorder(BorderFactory.createLineBorder(Color.black));
+        lblGold.setText("Amount of gold: " + playerList.getPlayerFromIndex(playernbr).getBalance());
+        lblGold.setFont(font);
+        lblGold.setHorizontalAlignment(SwingConstants.CENTER);
+        lblGold.setForeground(Color.black);
 
-		p4.setBounds(10, 154, 330, 50);
-		p4.setBorder(BorderFactory.createLineBorder(Color.black));
-		setLayout(null);
+        p2.add(lblGold);
+        add(p2);
 
-		lblName.setText(playerList.getPlayerFromIndex(playernbr).getName().toUpperCase());
-		lblName.setFont(font);
-		lblName.setHorizontalAlignment(SwingConstants.CENTER);
-		lblName.setOpaque(false);
-		lblName.setForeground(Color.white);
-		lblName.setBackground(playerList.getPlayerFromIndex(playernbr).getPlayerColor());
+        lblNetworth.setText("Total wealth: " + playerList.getPlayerFromIndex(playernbr).getNetWorth());
+        lblNetworth.setFont(font);
+        lblNetworth.setHorizontalAlignment(SwingConstants.CENTER);
+        p3.add(lblNetworth);
+        add(p3);
 
-		p1.add(lblName);
-		add(p1);
+        lblRank.setText("Player Rank: " + playerList.getPlayerFromIndex(playernbr).getPlayerRank());
+        lblRank.setFont(font);
+        lblRank.setHorizontalAlignment(SwingConstants.CENTER);
+        p4.add(lblRank);
+        add(p4);
 
-		lblGold.setText("Amount of gold: " + playerList.getPlayerFromIndex(playernbr).getBalance());
-		lblGold.setFont(font);
-		lblGold.setHorizontalAlignment(SwingConstants.CENTER);
-		lblGold.setForeground(Color.black);
-
-		p2.add(lblGold);
-		add(p2);
-
-		lblNetworth.setText("Total wealth: " + playerList.getPlayerFromIndex(playernbr).getNetWorth());
-		lblNetworth.setFont(font);
-		lblNetworth.setHorizontalAlignment(SwingConstants.CENTER);
-		p3.add(lblNetworth);
-		add(p3);
-
-		lblRank.setText("Player Rank: " + playerList.getPlayerFromIndex(playernbr).getPlayerRank());
-		lblRank.setFont(font);
-		lblRank.setHorizontalAlignment(SwingConstants.CENTER);
-		p4.add(lblRank);
-		add(p4);
-		propertyWindow.setBounds(10, 210, 335, 626);
-
-		propertyWindow.setPlayerAt(playernbr);
-		propertyWindow.addPlayerList(playerList);
-		add(propertyWindow);
-
-	}
-
+        propertyWindow = new PropertyWindow(playerList, playernbr);
+        propertyWindow.setBounds(10, 210, 335, 626);
+        propertyWindow.addTabs();
+        add(propertyWindow);
+    }
 }
--- src/View/EastGUI/PropertyWindow.java
+++ src/View/EastGUI/PropertyWindow.java
@@ -12,82 +12,41 @@ import Model.player.PlayerList;
  * @author Muhammad Abdulkhuder, Aevan Dino.
  */
 public class PropertyWindow extends JPanel {
+    private static final long serialVersionUID = 1L;
+    private PlayerList playerList;
+    private JTabbedPane tab;
+    private int playerAt;
 
-	
-	private static final long serialVersionUID = 1L;
-	private PlayerList playerList;
-	private JTabbedPane tab;
-	private PlayerProperties playerProperties;
+    public PropertyWindow(PlayerList playerList, int playerAt) {
+        this.playerList = playerList;
+        this.playerAt = playerAt;
 
-	private int playerAt;
+        setPreferredSize(new Dimension(330, 600));
+        setOpaque(false);
+        setLayout(null);
+        UIManager.put("TabbedPane.contentOpaque", false);
+        UIManager.put("TabbedPane.selected", Color.cyan);
 
-	private int[] size;
+        tab = new JTabbedPane();
+        tab.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
+        tab.setBorder(null);
+        tab.setBounds(0, 0, 330, 600);
+        add(tab);
+    }
 
-	/**
-	 * @param playerList
-	 * 
-	 *this method is used to update the panel
-	 */
-	
-	public PropertyWindow() {
+    /**
+     * this method loops the amount of players and adds tabs according to the number of
+     * properties
+     */
+    public void addTabs() {
+        tab.removeAll();
+        tab.setForeground(Color.white);
+        int size = playerList.getPlayerFromIndex(playerAt).getProperties().size();
 
-		setPreferredSize(new Dimension(330, 600));
-		setOpaque(false);
-		setLayout(null);
-		UIManager.put("TabbedPane.contentOpaque", false);
-		UIManager.put("TabbedPane.selected", Color.cyan);
-
-		tab = new JTabbedPane();
-		tab.setTabLayoutPolicy(JTabbedPane.SCROLL_TAB_LAYOUT);
-		tab.setBorder(null);
-		tab.setBounds(0, 0, 330, 600);
-		add(tab);
-
-	}
-	
-	public void addPlayerList(PlayerList playerList) {
-
-		this.playerList = playerList;
-		addtabs();
-
-	}
-
-
-	/**
-	 * this method loops the amount of players and adds tabs according to the number of 
-	 * properties
-	 */
-	public void addtabs() {
-
-		tab.removeAll();
-
-		tab.setForeground(Color.white);
-
-		size = new int[playerList.getPlayerFromIndex(getPlayerAt()).getProperties().size()];
-
-		for (int i = 0; i < size.length; i++) {
-
-			new PropertyWindow();
-			playerProperties = new PlayerProperties(playerList, getPlayerAt(), i);
-			tab.addTab("Property" + (i + 1), playerProperties);
-			tab.setBackgroundAt(i, playerList.getPlayerFromIndex(getPlayerAt()).getProperty(i).getColor());
-
-		}
-
-	}
-
-	/**
-	 * @return playerAT
-	 */
-	public int getPlayerAt() {
-		return playerAt;
-	} 
-
-	/**
-	 * @param playerAt
-	 */
-	public void setPlayerAt(int playerAt) {
-		this.playerAt = playerAt;
-	}
-
+        for (int i = 0; i < size; i++) {
+            PlayerProperties playerProperties = new PlayerProperties(playerList, playerAt, i);
+            tab.addTab("Property" + (i + 1), playerProperties);
+            tab.setBackgroundAt(i, playerList.getPlayerFromIndex(playerAt).getProperty(i).getColor());
+        }
+    }
 }
